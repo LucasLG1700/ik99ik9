bf9580ec21c14b7870f8f8e01038a8ea4814738f
LucasLG1700 <126296384+LucasLG1700@users.noreply.github.com>
2023-05-13 12:19:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+var  = document.getElementaryById("").value;
+.push();
+.sort();
+function procurarNome(){
+    var s= docuemnt.getElementaryById("").value;
+    var found=0;
+    var j;
+    for(j=0; j<.legth; j++)
+    {
+        if(s==[j]){
+        found=found+1;
+        }
+    }
+    document.getElementById("").innerHTML=""+found+"";
+    console.log(""+found+"");
+}
